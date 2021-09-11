7f23f8a5e82cda970d47c1f8db654fd1073bea38
Pablo Gomez <laquers@gmail.com>
2021-09-11 16:34:50 -0300

    tree dir en funcion

--- comandos_os.py
+++ comandos_os.py
@@ -1,8 +1,13 @@
 import os
 
-ruta = input("seleccione ruta: ")
+def tree_dir_files(ruta, ruta_salida_txt):
+    '''
+    Esta funcion saca en texto los files de un directorio en formato txt
+    '''
+    #cambiamos la ruta
+    os.chdir(ruta)
+    #generamos un txt con el tree_completo
+    os.system(f'tree /F > {ruta_salida_txt}\\files_dir.txt')
 
-#cambiamos la ruta
-os.chdir(ruta)
-#generamos un txt con el tree_completo
-os.system("tree /F > files_dir.txt")
+if __name__ == '__main__':
+    tree_dir_files('C:\\Users\\pgome\\Documents\\files_dir_excel\\SGS_SCC- Ensayos Geomecánicos Montonero','C:\\Users\\pgome\\Documents\\files_dir_excel')
